a2484183fec8b5d7ebe2c203c27fececa590715a
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-05 23:22:06 +0200

    add depends for session, and the offset / limit thing, also from docs

--- affirmations/main.py
+++ affirmations/main.py
@@ -10,7 +10,7 @@ from .models import (
     AffirmationRead,
 )
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Session, select
 
 app = FastAPI()
@@ -21,80 +21,89 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.post("/users/", response_model=UserRead)
-def create_user(user: UserCreate):
+def get_session():
     with Session(engine) as session:
-        db_user = User.from_orm(user)
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
-        return db_user
+        yield session
+
+
+@app.post("/users/", response_model=UserRead)
+def create_user(*, session: Session = Depends(get_session), user: UserCreate):
+    db_user = User.from_orm(user)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
 
 
 @app.get("/users/", response_model=List[UserRead])
-def read_users():
-    with Session(engine) as session:
-        users = session.exec(select(User)).all()
-        return users
+def read_users(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, lte=100)
+):
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
 
 
 @app.get("/users/{user_id}", response_model=UserRead)
-def read_user(user_id: int):
-    with Session(engine) as session:
-        user = session.get(User, user_id)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-        return user
+def read_user(*, session: Session = Depends(get_session), user_id: int):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
 
 @app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    with Session(engine) as session:
-        user = session.get(User, user_id)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-        session.delete(user)
-        session.commit()
-        return {"ok": True}
+def delete_user(*, session: Session = Depends(get_session), user_id: int):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return {"ok": True}
 
 
 @app.post("/affirmations/", response_model=AffirmationRead)
-def create_affirmation(affirmation: AffirmationCreate):
-    with Session(engine) as session:
-        db_affirmation = Affirmation.from_orm(affirmation)
+def create_affirmation(
+    *, session: Session = Depends(get_session), affirmation: AffirmationCreate
+):
+    db_affirmation = Affirmation.from_orm(affirmation)
 
-        user = session.get(User, affirmation.user_id)
-        if user is None:
-            raise HTTPException(status_code=400, detail="Not a valid user id")
+    user = session.get(User, affirmation.user_id)
+    if user is None:
+        raise HTTPException(status_code=400, detail="Not a valid user id")
 
-        session.add(db_affirmation)
-        session.commit()
-        session.refresh(db_affirmation)
-        return db_affirmation
+    session.add(db_affirmation)
+    session.commit()
+    session.refresh(db_affirmation)
+    return db_affirmation
 
 
 @app.get("/affirmations/", response_model=List[AffirmationRead])
-def read_affirmations():
-    with Session(engine) as session:
-        affirmations = session.exec(select(Affirmation)).all()
-        return affirmations
+def read_affirmations(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, lte=100)
+):
+    affirmations = session.exec(select(Affirmation).offset(offset).limit(limit)).all()
+    return affirmations
 
 
 @app.get("/affirmations/{affirmation_id}", response_model=AffirmationRead)
-def read_affirmation(affirmation_id: int):
-    with Session(engine) as session:
-        affirmation = session.get(Affirmation, affirmation_id)
-        if not affirmation:
-            raise HTTPException(status_code=404, detail="Affirmation not found")
-        return affirmation
+def read_affirmation(*, session: Session = Depends(get_session), affirmation_id: int):
+    affirmation = session.get(Affirmation, affirmation_id)
+    if not affirmation:
+        raise HTTPException(status_code=404, detail="Affirmation not found")
+    return affirmation
 
 
 @app.delete("/affirmations/{affirmation_id}")
-def delete_affirmation(affirmation_id: int):
-    with Session(engine) as session:
-        affirmation = session.get(Affirmation, affirmation_id)
-        if not affirmation:
-            raise HTTPException(status_code=404, detail="Affirmation not found")
-        session.delete(affirmation)
-        session.commit()
-        return {"ok": True}
+def delete_affirmation(*, session: Session = Depends(get_session), affirmation_id: int):
+    affirmation = session.get(Affirmation, affirmation_id)
+    if not affirmation:
+        raise HTTPException(status_code=404, detail="Affirmation not found")
+    session.delete(affirmation)
+    session.commit()
+    return {"ok": True}

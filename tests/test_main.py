7d122edc8f90d86f0005c419a019b3ef3deff5ae
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-05 23:25:58 +0200

    more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,10 +29,49 @@ def client_fixture(session: Session):
 
 
 def test_create_user(client: TestClient):
-    response = client.post("/users/", json={"name": "bob", "handle": "ua101"})
+    response = client.post("/users/", json={"name": "bob", "handle": "pybob"})
     data = response.json()
 
     assert response.status_code == 200
     assert data["name"] == "bob"
-    assert data["handle"] == "ua101"
+    assert data["handle"] == "pybob"
     assert data["id"] is not None
+
+
+def test_create_user_incomplete(client: TestClient):
+    # No hande
+    response = client.post("/users/", json={"name": "bob"})
+    assert response.status_code == 422
+
+
+def test_create_user_invalid(client: TestClient):
+    # handle has an invalid type
+    response = client.post(
+        "/users/",
+        json={
+            "name": "bob",
+            "handle": {"message": "Do you wanna know my secret identity?"},
+        },
+    )
+    assert response.status_code == 422
+
+
+def test_read_users(session: Session, client: TestClient):
+    user_1 = User(name="bob", handle="pybob")
+    user_2 = User(name="julian", handle="pyjul")
+    session.add(user_1)
+    session.add(user_2)
+    session.commit()
+
+    response = client.get("/users/")
+    data = response.json()
+
+    assert response.status_code == 200
+
+    assert len(data) == 2
+    assert data[0]["name"] == user_1.name
+    assert data[0]["handle"] == user_1.handle
+    assert data[0]["id"] == user_1.id
+    assert data[1]["name"] == user_2.name
+    assert data[1]["handle"] == user_2.handle
+    assert data[1]["id"] == user_2.id

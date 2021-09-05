a3b0baf17ca8732db39fb770dbe15bbf789c8e7e
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-05 22:45:35 +0200

    check_same_thread != postgres also only echo if in debug mode learned about strtobool - cool

--- affirmations/db.py
+++ affirmations/db.py
@@ -1,3 +1,4 @@
+from distutils.util import strtobool
 import os
 
 from dotenv import load_dotenv
@@ -5,9 +6,20 @@ from sqlmodel import create_engine, SQLModel
 
 load_dotenv()
 
+
+def _cast_boolean(value):
+    """
+    Helper to convert config values to boolean, got this from:
+    https://github.com/henriquebastos/python-decouple/blob/master/decouple.py
+    """
+    value = str(value)
+    return bool(value) if value == "" else bool(strtobool(value))
+
+
 database_url = os.environ["DATABASE_URL"]
-connect_args = {"check_same_thread": False}
-engine = create_engine(database_url, echo=True, connect_args=connect_args)
+debug = _cast_boolean(os.environ.get("DEBUG", False))
+
+engine = create_engine(database_url, echo=debug)
 
 
 def create_db_and_tables():

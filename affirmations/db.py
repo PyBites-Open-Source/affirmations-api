d7da6387d000de2ca2ae89c0a8935c727084262f
Bob Belderbos <bobbelderbos@gmail.com>
2024-10-04 15:07:35 +0200

    heroku workaround

--- affirmations/db.py
+++ affirmations/db.py
@@ -4,6 +4,9 @@ from sqlmodel import Session, SQLModel, create_engine
 DATABASE_URL = config("DATABASE_URL")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
+# heroku does not let me set the DATABASE_URL with the postgresql:// scheme
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL, echo=DEBUG)
 
